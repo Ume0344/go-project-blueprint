10329603ebcfccbca3fa5b2535c6fb1f53c43188
Um e Habiba <um-e.habiba@zeiss.com>
2025-08-21 14:40:06 +0200

    Add Dockerfile

--- skeleton/go.mod
+++ skeleton/go.mod
@@ -1,3 +1,3 @@
-module dev.azure.com/${{ values.projectName }}
+module go-project
 
-go 1.24.1
+go 1.22
--- skeleton/main.go
+++ skeleton/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("Successfully connected to the Go project!"))
+}
 
 func main() {
-	fmt.Println("Hello from Backstage scaffolded Go service!")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/healthz", healthzHandler)
+
+	// http://localhost:8080/healthz
+	fmt.Printf("🚀 Starting server on port %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
